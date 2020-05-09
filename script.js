5ab1e34ef9f9fc6aeed48094b94b9c8ddbce4be7
Raymond Hoong <raymondhoong@gmail.com>
2020-05-09 10:26:53 -0700

    almocomp finished

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-    
-function clickDropdown() {
-    document.querySelector('.dropdown-content').classList.toggle('show');
-    document.querySelector('.drop-btn').classList.toggle('toggle');
-}
+window.onload = function() {
+  // Close contact card when clicked outside
+  document.onclick = function(e) {
+    if(e.target.id === 'overlay' || e.target.id === 'nav'){
+      console.log(e.target.id);
+      document.querySelector('.contact-container').classList.remove('show');
+      document.querySelector('#contact-btn').classList.remove('active-contact');
+      document.querySelector('#contact-info').classList.remove('show');            
+    }
+  };
+
+  // Shadow on nav when scrolling
+  window.addEventListener('scroll', () => {
+    const nav = document.querySelector('#nav');
+    if (window.pageYOffset > 0){
+      nav.classList.add("shadow-header");
+    }
+    else{
+      nav.classList.remove('shadow-header');
+    }
+  });
+};
+
+function openContact() {
+  document.querySelector('.contact-container').classList.toggle('show');
+  document.querySelector('#contact-btn').classList.toggle('active-contact');
+};     
+
+function closeContact() {
+  document.querySelector('.contact-container').classList.toggle('show');
+  document.querySelector('#contact-btn').classList.toggle('active-contact');
+  document.querySelector('#contact-info').classList.remove('show');
+};
+
+function showInfo() {
+  document.querySelector('#contact-info').classList.toggle('show');
+};  
